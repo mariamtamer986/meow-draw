381df59f397a548ee8a797f0bef2c3cd464f3b9e
mariamtamer986 <mariamtamer986@gmail.com>
2025-12-20 15:35:31 +0100

    add mouse drag functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let isDragging = false;
 
 window.addEventListener('resize', function() {
     canvas.width = window.innerWidth;
@@ -14,26 +15,36 @@ const mouse = {
 }
 
 
-canvas.addEventListener('click', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    drawStroke();
+// canvas.addEventListener('click', function(event) {
+//     mouse.x = event.x;
+//     mouse.y = event.y;
+// })
+
+canvas.addEventListener('mousedown', function(event) {
+    isDragging = true;
 })
 
 canvas.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
+    if(isDragging){
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+})
+
+canvas.addEventListener('mouseup', function(event) {
+    if (isDragging)
+        isDragging = false;
 })
 
 function drawStroke() {
     ctx.fillStyle = 'red';
     ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    ctx.arc(mouse.x, mouse.y, 5, 0, Math.PI * 2);
     ctx.fill();
 }
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawStroke();
     requestAnimationFrame(animate);
 }
